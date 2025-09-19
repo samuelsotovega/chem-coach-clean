82bc66256db78569c93cc6bd03c85833ff8609cb
samuelsotovega <samuelsotovega@gmail.com>
2025-09-19 15:56:42 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>Chem Coach (clean)</h1>
+      <p>If you can read this, the new project is wired correctly.</p>
+      <p>
+        <a href="/lesson/L01">Test lesson route</a>
+      </p>
+    </div>
+  );
+}
